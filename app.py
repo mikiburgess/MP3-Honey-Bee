bd28b7a7c955e726c464b48649ba2e92b8d8a64a
mikiburgess <11767981+mikiburgess@users.noreply.github.com>
2024-02-18 19:52:14 +0000

    ADD: app.py added to project
    app.py added with home.html for testing

---
+++ app.py
@@ -0,0 +1,41 @@
+"""
+    MILESTONE PROJECT 3 by MIKHAILA BURGESS
+    "HONEY BEE"
+    - - - - - - - - - - - - - - - - - - - -
+    Web Application
+"""
+
+# IMPORT EXTERNAL PACKAGES
+import os
+import datetime
+from flask_pymongo import PyMongo
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from werkzeug.security import generate_password_hash, check_password_hash
+from bson.objectid import ObjectId  # Enables us to work with MongoDB Object IDs
+
+if os.path.exists("env.py"):
+    import env
+
+
+# INITIALISE WEB APPLICATION
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+
+# APPLICATION ENDPOINTS
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+# MAIN - RUNNING IN DEBUG MODE
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
